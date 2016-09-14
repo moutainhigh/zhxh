01018015b2c8093d42d4ea6c07b363ed647f1d95
wangf <needjava@163.com>
2016-09-14 16:32:06 +0800

    22

--- src/java/net/ussoft/zhxh/service/impl/PublicTradeBillService.java
+++ src/java/net/ussoft/zhxh/service/impl/PublicTradeBillService.java
@@ -6,37 +6,37 @@ import javax.annotation.Resource;
 
 import org.springframework.stereotype.Service;
 
-import net.ussoft.zhxh.dao.PublicBankgetListDao;
+import net.ussoft.zhxh.dao.PublicTradeBillDao;
 import net.ussoft.zhxh.model.PageBean;
-import net.ussoft.zhxh.model.Public_bankget_list;
-import net.ussoft.zhxh.service.IPublicBankgetListService;
+import net.ussoft.zhxh.model.Public_trade_bill;
+import net.ussoft.zhxh.service.IPublicTradeBillService;
 
 @Service
-public class PublicBankgetListService implements IPublicBankgetListService{
+public class PublicTradeBillService implements IPublicTradeBillService{
 
 	@Resource
-	PublicBankgetListDao bankgetListDao;
+	PublicTradeBillDao bankgetListDao;
 	
 	@Override
-	public Public_bankget_list getById(String id) {
+	public Public_trade_bill getById(String id) {
 		// TODO Auto-generated method stub
 		return null;
 	}
 
 	@Override
-	public List<Public_bankget_list> list() {
+	public List<Public_trade_bill> list() {
 		// TODO Auto-generated method stub
 		return null;
 	}
 
 	@Override
-	public List<Public_bankget_list> list(PageBean<Public_bankget_list> pageBean) {
+	public List<Public_trade_bill> list(PageBean<Public_trade_bill> pageBean) {
 		// TODO Auto-generated method stub
 		return null;
 	}
 
 	@Override
-	public int update(Public_bankget_list bankgetList) {
+	public int update(Public_trade_bill bankgetList) {
 		// TODO Auto-generated method stub
 		return 0;
 	}
@@ -48,7 +48,7 @@ public class PublicBankgetListService implements IPublicBankgetListService{
 	}
 
 	@Override
-	public Public_bankget_list insert(Public_bankget_list bankgetList) {
+	public Public_trade_bill insert(Public_trade_bill bankgetList) {
 		// TODO Auto-generated method stub
 		return null;
 	}
